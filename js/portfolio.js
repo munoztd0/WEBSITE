7250a31be3a050a8a0a8d863f4a911b436821e30
David Munoz <david.munoztord@mailbox.org>
2022-08-21 16:09:20 +0000

    done_again

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,26 @@
-$(function() {
+$(function () {
 
-    var all_portfolio_items = $("#portfolio-items div");
+  var all_portfolio_items = $("#portfolio-items div");
 
-    $("#portfolio-button-container button").click(function(e) {
-        var category = $(this).data('portfolio-section');
-        console.log("Show " + category);
+  $("#portfolio-button-container button").click(function(e) {
+    var category = $(this).data('portfolio-section');
+    console.log("Show " + category);
 
-        $("#portfolio-button-container button").removeClass('active');
-        $(this).addClass('active');
+    $("#portfolio-button-container button").removeClass('active');
+    $(this).addClass('active');
 
-        if (category == 'all') {
-            all_portfolio_items.addClass('show');
-            return;
+    if (category == 'all') {
+      all_portfolio_items.addClass('show');
+      return;
+    }
 
-        }
-
-        all_portfolio_items.each(function() {
-            if ($(this).hasClass(category)) {
-                $(this).addClass('show', 1000);
-            } else {
-                $(this).removeClass('show', 1000);
-            }
-        });
-
+    all_portfolio_items.each(function() {
+      if ($(this).hasClass(category)) {
+        $(this).addClass('show', 1000);
+      } else {
+        $(this).removeClass('show', 1000);
+      }
     });
+
+  });
 });
